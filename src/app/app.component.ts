1c766ac3418bf9b0509c587f9e146addb07bb712
Ayush Kumar <ayush.krsh9@gmail.com>
2024-05-12 09:53:35 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RepositoryComponent } from './repository/repository.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SearchComponent,ProfileComponent,RepositoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  
+  { path: 'search', component: SearchComponent },
+  // Add more routes here if you have additional components to render
+];
